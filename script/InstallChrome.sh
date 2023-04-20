c55f25c282e1dc763cb9be433440ab15c8482d63
Kashish <kkaswan@gmail.com>
2023-04-20 10:59:56 -0400

    modified yml without dir

--- script/InstallChrome.sh
+++ script/InstallChrome.sh
@@ -6,3 +6,4 @@ apt-get install wget
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt -y install ./google-chrome-stable_current_amd64.deb
 apt-get -y install maven
+apt-get -y install cucumber
