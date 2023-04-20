39892e96e472f4954439f0f0237db8998eca9a2a
Kashish <kkaswan@gmail.com>
2023-04-20 10:47:30 -0400

    modified yml without dir

--- script/InstallChrome.sh
+++ script/InstallChrome.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
+apt-get update
+apt-get -y install curl
 apt-get install wget
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install ./google-chrome-stable_current_amd64.deb
